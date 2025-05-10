70e431d957464fb04065f97dc0475e63d561fea5
Sean CHen <Seanchendu@gmail.com>
2025-05-09 22:02:25 -0500

    oh mmy faking god

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def home():
     return render_template('home.html', user=current_user)
 
 @app.route('/test')
-def home():
+def test():
     return render_template('test.html', user=current_user)
 
 @app.route('/products')
