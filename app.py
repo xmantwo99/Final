96517d7a74c6d7c5ee4d310986ebe200c575a991
Sean CHen <Seanchendu@gmail.com>
2025-05-12 01:18:58 -0500

    dotenv

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pyodbc
 from auth import auth_bp
 from google.oauth2 import id_token
 from google.auth.transport import requests as google_requests
+import dotenv
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
